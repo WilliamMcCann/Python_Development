65cafad95c8114edc2efc14a84711c9301c770b5
William McCann <nwilliammccann@gmail.com>
2016-08-31 10:57:57 -0700

    RuntimeError: maximum recursion depth exceeded

--- addition_recursion.py
+++ addition_recursion.py
@@ -3,7 +3,11 @@
 print "This is a program that multiplies two numbers."
 n = (int(raw_input('Enter the first number: ')))
 m = (int(raw_input('Enter the first number: ')))
-print n
-print m
-print type(n)
-print n * m
+
+def multiply(a,b):
+	if a > 1:
+		return (a + (multiply(a,b-1)))
+	if a == 1:
+		return 1
+
+print multiply(n,m)
