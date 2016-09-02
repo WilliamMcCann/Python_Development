c08aa353246d8afd646b8ee1b9dd2692847887a0
William McCann <nwilliammccann@gmail.com>
2016-09-02 14:27:07 -0700

    program functions properly

--- diagonals_difference.py
+++ diagonals_difference.py
@@ -2,4 +2,27 @@
 
 print "This program takes a matrix and prints the absolute difference of the sums of the matrix diagonals"
 
+n = int(raw_input("Please enter the dimensions of an NxN matrix: ").strip())
+print type(n)
+print n
 
+matrix = []
+for i in range(n):
+    print i
+    temp = map(int, raw_input("Please enter a line from the NxN matrix: ").strip().split())
+    matrix.append(temp)
+
+diag1 = []
+for j in range(n):
+    a = matrix[j][j]
+    diag1.append(a)
+s1 = sum(diag1)
+
+diag2 = []
+for k in range(n):
+    b = matrix[k][n-1]
+    n = n-1
+    diag2.append(b)
+s2 = sum(diag2)
+    
+print abs(s1 - s2)
