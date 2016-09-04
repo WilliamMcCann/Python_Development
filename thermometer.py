2a7240faa09190875691a9f5cb8d7c0ddff64bd2
William McCann <nwilliammccann@gmail.com>
2016-09-03 21:53:56 -0700

    rough outline of thermometer

---
+++ thermometer.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+print"This program converts temperatures between Fahrenheit, Celsius, and Bug chirps."
+temp, unit = raw_input("Please enter the temperature and the unit (F, C, or B): ").split()
+temp = int(temp)
+print temp
+print unit
