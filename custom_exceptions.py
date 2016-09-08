0ff628ccfb9ec0ea93963215eddf986a6971588f
William McCann <nwilliammccann@gmail.com>
2016-09-08 11:51:59 -0700

    custom exceptions works properly

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -5,4 +5,4 @@ n,p = map(int, raw_input("Enter an integer, and then an integer two which you'd 
 if n < 0 or p < 0:
 	raise Exception("n and p should be non-negative")
 else:
-        return(n**p)
+        print(n**p)
