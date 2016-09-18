a9f40d6f9dceb3bc43d8d489e0e903518b9cdae8
William McCann <nwilliammccann@gmail.com>
2016-09-18 16:14:19 -0700

    adding simple fizzbuzz

---
+++ fizzbuzz_simple.py
@@ -0,0 +1,11 @@
+#!/user/bin/env/ python
+
+for x in xrange(1, 101):
+	if x % 3 == 0 and x % 5 == 0:
+		print "FizzBuzz"
+	elif x % 3 == 0:
+		print "Fizz"
+	elif x % 5 == 0:
+		print "Buzz"
+	else:
+		print x
