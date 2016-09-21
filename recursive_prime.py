72b891d8324a37e9679ed196ac340af411931c8b
William McCann <nwilliammccann@gmail.com>
2016-09-20 19:45:34 -0700

    adding recursive primes program

---
+++ recursive_prime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+def work(x,n):
+        print("In work")
+	if x == 1:
+		return True
+#        elif x == 2:
+ #               return True
+        else:
+		if n % x == 0:
+                	return False
+	        if n % x != 0:
+        		return(work(x-1, n))
+
+def prime(n):
+	print("In prime")
+	x = (n / 2) + 1
+	if n == 1:
+		print "Not Prime"
+	elif n == 2:
+		print "Prime"
+	elif (work(x,n) == True):
+		print "Prime"
+	else:
+		print "Not Prime"
+
+prime(int(raw_input()))
