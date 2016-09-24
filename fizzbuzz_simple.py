87824fdf0faa788de4564bac3cf60d962139e8ab
William McCann <nwilliammccann@gmail.com>
2016-09-23 19:32:25 -0700

    optimized fizzbuzz

--- fizzbuzz_simple.py
+++ fizzbuzz_simple.py
@@ -1,7 +1,8 @@
 #!/user/bin/env/ python
 
 for x in xrange(1, 101):
-	if x % 3 == 0 and x % 5 == 0:
+#	if x % 3 == 0 and x % 5 == 0:
+	if x % 15 == 0:
 		print "FizzBuzz"
 	elif x % 3 == 0:
 		print "Fizz"
